16ed503662a6df3e3f941ed146e28b21f864391c
xuwudatianshi <2055507639@qq.com>
2025-09-03 11:11:47 +0800

    记录

---
+++ src/views/AboutView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <h1 @click="test" style="cursor: pointer;">This is an about page</h1>
+  </div>
+</template>
+<script setup>
+  let obj = new Proxy(
+    {},
+    {
+      get(target, prop,receiver) {
+        console.log('get', target,prop,receiver,target==receiver)
+        return prop in target ? target[prop] : 37
+      },
+    }
+  )
+  obj.a = 1
+  const test = () => {
+    console.log(obj.a)
+  }
+</script>
+<style scoped>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
