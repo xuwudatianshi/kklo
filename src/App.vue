4b06bd2063028d2f27664f8fd465bb5fd864a974
xuwudatianshi <2055507639@qq.com>
2025-09-23 14:31:43 +0800

    合并分支

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/">Home2</RouterLink>
         <RouterLink to="/about">About2</RouterLink>
       </nav>
     </div>
